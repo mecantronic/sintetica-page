5d3571c95d4472f04573f314a7053a6ef13a644c
ortega-pablo <87724507+ortega-pablo@users.noreply.github.com>
2023-10-30 14:55:30 +0000

    Build (2536270): Merge pull request #39 from mecantronic/prebuild
    Prebuild

--- src/styles/pages/voiceCloning.style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const VoiceWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-  min-height: 100vh;
-`;
