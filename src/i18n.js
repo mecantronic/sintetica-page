7be4ea23b80a4ecd6c11c53cf86bfb8e88216d52
Pablo Ortega <p.ortegariera@gmail.com>
2024-05-03 12:25:19 -0300

    helmet unused dependency deleted and i18n config fixed

--- src/i18n.js
+++ src/i18n.js
@@ -1,9 +1,23 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
-i18n.use(Backend).use(initReactI18next).init({
-  fallbackLng: 'es',
-});
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'es',
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+    ns: ['data', 'voiceCloning'],
+    defaultNS: 'data',
+  });
 
 export default i18n;
