812000c1f4d1fe8c702c87113db5c5bf0410cae7
altobellidardo <altobellidardo@gmail.com>
2024-09-11 12:37:06 +0000

    feat: use heroBanner component in tts

--- src/components/tts/TtsBannerSection.jsx
+++ src/components/tts/TtsBannerSection.jsx
@@ -1,7 +1,19 @@
+import { useTranslation } from 'react-i18next';
 import HeroSection from '../shared/HeroSection/HeroSection';
 
 function TtsBanner() {
-  return <HeroSection keyName="contentExperienceTTS" />;
+  const { t } = useTranslation('tts');
+
+  const handleSection = () => {}
+
+  return <HeroSection
+    imgSrc="assets/tts/bannerTts.svg"
+    imgAlt=""
+    heroTitle={t('heroSection.title')}
+    heroResume={t('heroSection.resume')}
+    buttonText={t('heroSection.buttonText')}
+    handleClick={handleSection}
+  />
 }
 
 export default TtsBanner;
