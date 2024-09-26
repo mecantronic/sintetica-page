69002c4d32ca5c4aea1284e1e81afbd622bff57f
altobellidardo <altobellidardo@gmail.com>
2024-09-26 14:43:04 +0000

    fix: control props

--- src/components/shared/customSelect/CustomSelect.jsx
+++ src/components/shared/customSelect/CustomSelect.jsx
@@ -1,16 +1,18 @@
 import PropTypes from "prop-types"
 import StyledCustomSelect from "./CustomSelect.styles"
 
-function CustomSelect ({ children, ...props }) {
+function CustomSelect ({ children, value, onChange }) {
   return (
-    <StyledCustomSelect {...props}>
+    <StyledCustomSelect value={value} onChange={onChange}>
       {children}
     </StyledCustomSelect>
   )
 }
 
 CustomSelect.propTypes = {
-  children: PropTypes.element
+  children: PropTypes.arrayOf(PropTypes.element),
+  value: PropTypes.string,
+  onChange: PropTypes.func
 }
 
 export default CustomSelect
