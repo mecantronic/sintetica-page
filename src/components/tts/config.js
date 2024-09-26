fbc46d8e0121f336452d33c7ae1ad07baccab2ce
altobellidardo <altobellidardo@gmail.com>
2024-09-26 12:32:42 +0000

    style: separate global parameters

---
+++ src/components/tts/config.js
@@ -0,0 +1,13 @@
+export const LANGUAGES = [
+  { value: 'es-ri', label: 'Español rioplatense' },
+  { value: 'en', label: 'English' }
+];
+
+export const SPEACHERS = [
+  { value: 'Micaela', label: 'Micaela' },
+  { value: 'Florencia', label: 'Florencia' },
+  { value: 'Rocio', label: 'Rocío' },
+  { value: 'Pedro', label: 'Pedro' },
+  { value: 'Pablo', label: 'Pablo' },
+  { value: 'Juan', label: 'Juan' }
+]
